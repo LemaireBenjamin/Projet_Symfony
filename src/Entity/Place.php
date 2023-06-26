28e3d405861be3128d53dfc118a6ea20bf90cc7e
Bertrand_GitHub <bertrand.meunier@protonmail.com>
2023-06-26 16:52:56 +0200

    made relation

--- src/Entity/Place.php
+++ src/Entity/Place.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\PlaceRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: PlaceRepository::class)]
@@ -27,6 +29,17 @@ class Place
 
     #[ORM\Column(nullable: true)]
     private ?float $longitude = null;
+
+    #[ORM\OneToMany(mappedBy: 'place', targetEntity: Activity::class)]
+    private Collection $activities;
+
+    #[ORM\ManyToOne(inversedBy: 'places')]
+    private ?City $city = null;
+
+    public function __construct()
+    {
+        $this->activities = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -89,6 +102,48 @@ class Place
     public function setLongitude(?float $longitude): static
     {
         $this->longitude = $longitude;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Activity>
+     */
+    public function getActivities(): Collection
+    {
+        return $this->activities;
+    }
+
+    public function addActivity(Activity $activity): static
+    {
+        if (!$this->activities->contains($activity)) {
+            $this->activities->add($activity);
+            $activity->setPlace($this);
+        }
+
+        return $this;
+    }
+
+    public function removeActivity(Activity $activity): static
+    {
+        if ($this->activities->removeElement($activity)) {
+            // set the owning side to null (unless already changed)
+            if ($activity->getPlace() === $this) {
+                $activity->setPlace(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getCity(): ?City
+    {
+        return $this->city;
+    }
+
+    public function setCity(?City $city): static
+    {
+        $this->city = $city;
 
         return $this;
     }
--- src/Entity/Status.php
+++ src/Entity/Status.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\StatusRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: StatusRepository::class)]
@@ -18,6 +20,14 @@ class Status
 
     #[ORM\Column]
     private ?int $status_label = null;
+
+    #[ORM\OneToMany(mappedBy: 'status', targetEntity: Activity::class)]
+    private Collection $activities;
+
+    public function __construct()
+    {
+        $this->activities = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -44,6 +54,36 @@ class Status
     public function setStatusLabel(int $status_label): static
     {
         $this->status_label = $status_label;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Activity>
+     */
+    public function getActivities(): Collection
+    {
+        return $this->activities;
+    }
+
+    public function addActivity(Activity $activity): static
+    {
+        if (!$this->activities->contains($activity)) {
+            $this->activities->add($activity);
+            $activity->setStatus($this);
+        }
+
+        return $this;
+    }
+
+    public function removeActivity(Activity $activity): static
+    {
+        if ($this->activities->removeElement($activity)) {
+            // set the owning side to null (unless already changed)
+            if ($activity->getStatus() === $this) {
+                $activity->setStatus(null);
+            }
+        }
 
         return $this;
     }
