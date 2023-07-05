d24623bf6875ebd4ca0a6762c884994bfa925df9
YSonz <sonzy2@gmail.com>
2023-07-05 13:59:54 +0200

    test suite phpunit

--- tests/Entity/PaticipantTest.php
+++ tests/Entity/PaticipantTest.php
@@ -2,7 +2,34 @@
 
 namespace App\Tests\Entity;
 
-class PaticipantTest
+use App\Entity\Activity;
+use App\Entity\Participant;
+use PHPUnit\Framework\TestCase;
+
+class PaticipantTest extends TestCase
 {
+    public function testActivities()
+    {
+        $activity= new Activity();
+        $art="Test";
+
+        $activity->setName("$art");
+        $this->assertEquals("Test",$activity->getName());
+        //$activity->setName("sortie plage");
+        //$this->assertEquals("sortie plage",$activity->getName());
+    }
+
+    public function testInscriptionAvantDateCloture()
+    {
+        $dateCloture = new \DateTime('+7 days'); // Date de clôture dans une semaine
+        $sortie = new Activity($dateCloture);
+        $participant = new Participant();
+
+        $result = $sortie->addParticipant($participant);
+
+        $this->assertTrue((bool)$result, "L'inscription devrait être réussie");
+        $this->assertContains($participant, $sortie->getParticipants(), "Le participant devrait être ajouté à la liste des participants");
+    }
+
 
 }
